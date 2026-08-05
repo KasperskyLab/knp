7ecf8a25b425dfe702a916e36e192231dcdbbe7e
NeZero2020 <knezero1@gmail.com>
2026-08-05 17:21:53 +0300

    add initial comments: #219

--- knp/tests/framework/visualize_network_test.cpp
+++ knp/tests/framework/visualize_network_test.cpp
@@ -1,3 +1,24 @@
+/**
+ * @file visualize_network_test.cpp
+ * @brief Functions for subgraph visualization.
+ * @kaspersky_support Kirill L.
+ * @date 05.08.2026
+ * @license Apache 2.0
+ * @copyright © 2024 AO Kaspersky Lab
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <knp/backends/cpu-single-threaded/backend.h>
 #include <knp/framework/model.h>
 #include <knp/framework/network.h>
@@ -8,7 +29,6 @@
 #include <tests_common.h>
 
 #include <filesystem>
-#include <fstream>
 #include <string>
 
 using BLIFATParams = knp::neuron_traits::neuron_parameters<knp::neuron_traits::BLIFATNeuron>;
